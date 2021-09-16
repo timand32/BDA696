fddf717c52bdd02050ecf0a46ad59c47bb9654b2
Timothy Andersen <timandersen32@gmail.com>
2021-09-15 21:00:04 -0700

    Add _generate_scatterplot() to plots.py.

--- apps/iris/plots.py
+++ apps/iris/plots.py
@@ -4,17 +4,53 @@ import sys
 
 import data
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.subplots as psp
 
 
-def generate_plots(iris_df: pd.DataFrame):
-    return list()
+def _generate_scatterplot(iris_df: pd.DataFrame) -> go.Figure:
+    figure = psp.make_subplots(
+        rows=1,
+        cols=2,
+        subplot_titles=["Sepal Width vs. Length", "Pedal Width vs. Length"],
+    )
+    sepal_plot = px.scatter(
+        data_frame=iris_df,
+        x="sepal_width",
+        y="sepal_length",
+        color="class",
+    )
+    pedal_plot = px.scatter(
+        data_frame=iris_df, x="pedal_width", y="pedal_length", color="class"
+    )
+
+    # Stack exchange question 60633891 really helped here.
+    for trace in sepal_plot.data:
+        figure.add_trace(trace, 1, 1)
+    for trace in pedal_plot.data:
+        figure.add_trace(trace, 1, 2)
+
+    # Stack exchange 58849925 helped here understanding editing Figures.
+    figure.layout["xaxis"]["title"] = "Sepal Width"
+    figure.layout["yaxis"]["title"] = "Sepal Length"
+    figure.layout["xaxis2"]["title"] = "Pedal Width"
+    figure.layout["yaxis2"]["title"] = "Pedal Length"
+
+    return figure
+
+
+def generate_plots(iris_df: pd.DataFrame) -> list():
+    plots = list()
+    plots.append(_generate_scatterplot(iris_df=iris_df))
+    return plots
 
 
 def main() -> int:
     iris_df = data.load_data()
     plots = generate_plots(iris_df=iris_df)
     for plot in plots:
-        print(plot)
+        plot.show()
     return 0
 
 
