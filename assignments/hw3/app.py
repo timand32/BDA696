63495ed4f8fe94aad7a861d64b7b8a318f6d1388
Timothy Andersen <timandersen32@gmail.com>
2021-10-07 16:46:01 -0700

    Implement custom transformer.

--- assignments/hw3/app.py
+++ assignments/hw3/app.py
@@ -2,16 +2,19 @@
 """
 import sys
 
-import calc
+# import calc
 import data
 from pyspark.sql import SparkSession
+from transformers import RollingBATransform
 
 
 def main() -> int:
     spark = SparkSession.builder.master("local[*]").getOrCreate()
     mstr_df = data.load_master_df(spark)
-    rlng_df = calc.calculate_ba(spark, mstr_df)
-    rlng_df.show()
+    # rlng_df = calc.calculate_ba(spark, mstr_df)
+    transformer = RollingBATransform()
+    rlng_df = transformer.transform(mstr_df, {})
+    rlng_df.show(n=5)
     return 0
 
 
