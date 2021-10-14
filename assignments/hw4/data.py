4269f5486bd0b56c2afde5087d7788b1573e78b0
Timothy Andersen <timandersen32@gmail.com>
2021-10-13 22:42:51 -0700

    Implement data module.
    Module mainly for testing.
    
    Also add seaborn to quickly import test datsets.

---
+++ assignments/hw4/data.py
@@ -0,0 +1,66 @@
+"""Module importing several datasets.
+
+Meant mostly for testing purposes.
+"""
+
+import sys
+
+import seaborn
+
+mpg = seaborn.load_dataset(name="mpg")
+tips = seaborn.load_dataset(name="tips")
+titanic = seaborn.load_dataset(name="titanic").dropna()
+
+test_cases = [
+    (
+        mpg,
+        [
+            "cylinders",
+            "displacement",
+            "horsepower",
+            "weight",
+            "acceleration",
+            "origin",
+            "name",
+        ],
+        "mpg",
+    ),
+    (
+        tips,
+        [
+            "total_bill",
+            "sex",
+            "smoker",
+            "day",
+            "time",
+            "size",
+        ],
+        "tip",
+    ),
+    (
+        titanic,
+        [
+            "pclass",
+            "sex",
+            "age",
+            "sibsp",
+            "embarked",
+            "class",
+        ],
+        "survived",
+    ),
+]
+"""Test cases for feature score application.
+"""
+
+
+def main() -> int:
+    for df, predictors, response in test_cases:
+        features = list() + predictors
+        features.append(response)
+        print(df[features])
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
