5aec0aa776985566e88418590ff04f0d7075dcc8
Timothy Andersen <timandersen32@gmail.com>
2021-11-11 17:52:08 -0800

    Add data module for importing and cleaning data.

--- assignments/hw5/report.py
+++ assignments/hw5/report.py
@@ -2,8 +2,20 @@
 """
 import sys
 
+import data
+
 
 def main() -> int:
+    # Load data
+    y = data.load_y()
+    X0 = data.load_rolling_100_pitcher_X()
+    X1 = data.load_prior_career_pitcher_X()
+    # make sure we only keep y we have X for.
+    y = y.loc[y.index.isin(X0.index)]
+    # same for X with y
+    X0 = X0.loc[X0.index.isin(y.index)]
+    X1 = X1.loc[X1.index.isin(y.index)]
+    print(f"{y}\n{X0}\n{X1}")
     return 0
 
 
